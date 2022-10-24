62078be9f2c0de1520e785fb42085da54267d28d
Andreo2 <andreo2.filippov00@gmail.com>
2022-10-24 20:45:07 +0300

    allgather update

--- second_task/main.cpp
+++ second_task/main.cpp
@@ -1,45 +1,40 @@
 #define _USE_MATH_DEFINES
 #include <iostream>
-#include <random>
+#include <ctime>
+#include <cstdlib>
 #include <cmath>
 #include "mpi.h"
-#define N 25
 #define TARGET 4.0*M_PI/3.0
 int main(int argc, char *argv[]){
   int rank, size;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  double y, z, volume;
-  volume =  2.0*2.0*2.0/size;
+  double y, z, volume = 8.0/size;
+  int N = 100;
   //define random
-  std::random_device rand_dev;
-  std::mt19937 gen(rand_dev());
-  std::uniform_real_distribution<> dis_y(-1, 1);
-  std::uniform_real_distribution<> dis_z(-1, 1);
-  double tol = 1.0e-04, eps = 0.0, buf = 0.0, acc_volume, start_time, proc_sum = 0.0, proc_volume = 0.0;
+  double tol = std::atof(argv[1]), eps = 0.0, acc_volume = 0.0, start_time, proc_sum = 0.0;
   int iter = 1, stop = 1;
 
   start_time = MPI_Wtime();
+  srand(rank+1);
   while(stop)
   {
-      for(int i = 0; i < N; ++i)
-      {
-          y = dis_y(gen);
-          z = dis_z(gen);
-          if (y*y + z*z <= 1)
-              proc_sum += sqrt(y*y + z*z);
-      }
-      proc_sum = volume * proc_sum;
-      MPI_Allreduce(&proc_sum, &proc_volume, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
-      acc_volume += proc_volume;
-      eps = std::abs(TARGET - acc_volume/(iter*N));
-      if (eps < tol) {
-          stop = 0;
-          acc_volume = eps + TARGET;
-      }
-      proc_sum = 0.0;
-      iter++;
+    for(int i = 0; i < N; ++i)
+    {
+      y = -1 + 2.0*(double)rand()/(RAND_MAX+1.0);
+      z = -1 + 2.0*(double)rand()/(RAND_MAX+1.0);
+      if (y*y + z*z <= 1)
+        proc_sum += sqrt(y*y + z*z);
+    }
+    MPI_Allreduce(&proc_sum, &acc_volume, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    eps = std::abs(TARGET - volume*acc_volume/(iter*N));
+    if (eps < tol)
+    {
+      stop = 0;
+      acc_volume = TARGET + eps;
+    }
+    iter++;
   }
   double end_time = MPI_Wtime();
   double result_time, time = end_time - start_time;
